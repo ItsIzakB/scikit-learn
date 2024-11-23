1014b5b22d44c8c509a0e695fa20f85d72adef7b
Kazi Priom <109478175+ItsIzakB@users.noreply.github.com>
2024-11-22 20:26:41 -0600

    Update scikit.py

--- scikit.py
+++ scikit.py
@@ -6,9 +6,14 @@ diabetes = load_diabetes()
 X = diabetes.data
 y = diabetes.target
 
-print("X: \n", X[:5])
-print("y: \n", y[:5])
+# print("X: \n", X[:5])
+# print("y: \n", y[:5])
+#
+# print("Feature: \n", diabetes.feature_names)
+# print("Dataset Description: \n", diabetes.DESCR)
 
-print("Feature: \n", diabetes.feature_names)
-print("Dataset Description: \n", diabetes.DESCR)
 
+from sklearn.neighbors import KNeighborsRegressor
+
+mod = KNeighborsRegressor() #model with no learning
+mod.predict(X) #has not been fitted yet so will result in error
