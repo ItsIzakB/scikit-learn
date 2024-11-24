cbd5d8947d4ba2e39d8e736cae4b4a8c325e4f65
izak <kazipriom@gmail.com>
2024-11-23 19:36:06 -0600

    added pre-processing and pipeline

--- pre_processing_and_pipeline.py
+++ pre_processing_and_pipeline.py
@@ -1,4 +1,27 @@
-from sklearn.datasets import KNeighborsRegressor
+from sklearn.neighbors import KNeighborsRegressor
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import Pipeline
+
+
+from sklearn.datasets import load_diabetes
+
+diabetes = load_diabetes()
+X = diabetes.data
+y=diabetes.target
+
+#preprocessing and pipeline
+pipe = Pipeline([
+                ("scale", StandardScaler()),
+                 ("model", KNeighborsRegressor())
+                 ])
+
+pipe.fit(X,y)
+
+pred = pipe.predict(X)
+
+plt.plot(pred,y, 'o')
+plt.xlabel('pred')
+plt.ylabel('actual')
+
+plt.show()
