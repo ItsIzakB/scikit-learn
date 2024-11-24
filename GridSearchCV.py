3761067d56f8aa7056aa8d9039053b99c3ec498a
izak <kazipriom@gmail.com>
2024-11-24 17:00:15 -0600

    showed how the model performed

--- GridSearchCV.py
+++ GridSearchCV.py
@@ -3,24 +3,23 @@ from sklearn.neighbors import KNeighborsRegressor
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import GridSearchCV
-import matplotlib.pyplot as plt
+import pandas as pd
+from IPython.display import display
 
-# Load diabetes dataset
 diabetes = load_diabetes()
 
 X = diabetes.data
 y = diabetes.target
 
-# Create the pipeline with instantiated objects for both steps
 pipe = Pipeline([('scale', StandardScaler()), ('model', KNeighborsRegressor())])
 
-# Perform grid search with the correct parameter grid
 param_grid = {'model__n_neighbors': [1, 2, 3, 4, 5]}
 
 mod = GridSearchCV(estimator=pipe, param_grid=param_grid, cv=3, error_score='raise')
 
-# Fit the model
 mod.fit(X, y)
 
-# Output the results
-print(mod.cv_results_)
+
+dataframe = pd.DataFrame(mod.cv_results_)
+#displayed in jupyter notebook
+display(dataframe)
