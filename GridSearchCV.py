5436d934cd359cf4e0aaa032a1c19b63a0b68b13
izak <kazipriom@gmail.com>
2024-11-24 16:26:25 -0600

    added GridSearchCV.py

---
+++ GridSearchCV.py
@@ -0,0 +1,7 @@
+from sklearn.datasets import load_diabetes
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import GridSearchCV
+import matplotlib.pyplot as plt
+
