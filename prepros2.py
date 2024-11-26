6790b45ead2485ee4655d228802731e8e2cbda95
izak <kazipriom@gmail.com>
2024-11-25 22:08:19 -0600

    using quantiletransformers instead of standardscalar

--- prepros2.py
+++ prepros2.py
@@ -15,12 +15,20 @@ y = data['z'] == 'a'
 # plt.scatter(X[:,0], X[:,1], c=y)
 # plt.show()
 
-#scaling with StandardScalar
+#scaling with StandardScalar -- issue is with outliers
 
-from sklearn.preprocessing import StandardScaler
+# from sklearn.preprocessing import StandardScaler
+#
+# X_new = StandardScaler().fit_transform(X)
+#
+# plt.scatter(X_new[:,0], X_new[:,1], c=y)
+# plt.show()
 
-X_new = StandardScaler().fit_transform(X)
+#scaling with QuantileTransformer
+
+from sklearn.preprocessing import QuantileTransformer
+
+X_new = QuantileTransformer(n_quantiles=100).fit_transform(X)
 
 plt.scatter(X_new[:,0], X_new[:,1], c=y)
 plt.show()
-
