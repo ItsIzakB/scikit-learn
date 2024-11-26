36763341edeb2a8c75dc2695dae9102231118a0f
izak <kazipriom@gmail.com>
2024-11-25 21:53:50 -0600

    prepos2 data config

--- prepros2.py
+++ prepros2.py
@@ -7,3 +7,10 @@ import matplotlib.pylab as plt
 data = pd.read_csv('drawndata1.csv')
 
 print(data.head(3))
+
+
+X = data[['x', 'y']].values
+y = data['z'] == 'a'
+
+plt.scatter(X[:,0], X[:,1], c=y)
+plt.show()
