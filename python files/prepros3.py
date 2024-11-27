830d4ff634151589768866849106eff40251e08d
izak <kazipriom@gmail.com>
2024-11-26 18:25:32 -0600

    new python file

---
+++ python files/prepros3.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pylab as plt
+from sklearn.preprocessing import QuantileTransformer
+from sklearn.pipeline import Pipeline
+
+data = pd.read_csv('/CSV file/drawndata2.csv')
+
