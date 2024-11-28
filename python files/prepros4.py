ef1e2a32905207d5599d8ebcc330889327678ed2
izak <kazipriom@gmail.com>
2024-11-27 19:04:27 -0600

    onehotencoder work

--- python files/prepros4.py
+++ python files/prepros4.py
@@ -7,6 +7,5 @@ arr = np.array(['low', 'low', 'high', 'medium']).reshape(-1,1)
 from sklearn.preprocessing import OneHotEncoder
 
 encoder = OneHotEncoder(sparse_output=False, handle_unknown= 'ignore')
-encoder.fit_transform(arr)
-
-print(encoder)
+encoded_data = encoder.fit_transform(arr)
+print(encoded_data)
