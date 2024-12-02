63e5ed1ef79b17504d865fb6d616b381b77923c6
izak <kazipriom@gmail.com>
2024-12-01 19:14:26 -0600

    created model

--- python files/metrics.py
+++ python files/metrics.py
@@ -10,3 +10,10 @@ file = pd.read_csv('../CSV_files/creditcard.csv')
 X = file.drop(columns=['Time', 'Amount', 'Class'])
 y = file['Class'].values
 
+from sklearn.linear_model import LinearRegression
+
+mod = LinearRegression()
+
+mod.fit(X, y)
+
+pred = mod.predict(X)
