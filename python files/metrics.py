b6444a6af4718061d474ddbf3d9933c5548ce002
izak <kazipriom@gmail.com>
2024-12-01 19:18:23 -0600

    model is working. Its not supposed to

--- python files/metrics.py
+++ python files/metrics.py
@@ -10,6 +10,9 @@ file = pd.read_csv('../CSV_files/creditcard.csv')
 X = file.drop(columns=['Time', 'Amount', 'Class'])
 y = file['Class'].values
 
+print(f'shape of x: {X.shape} and shape of y: {y.shape}, '
+      f'fraud = {y.sum()}')
+
 from sklearn.linear_model import LinearRegression
 
 mod = LinearRegression()
