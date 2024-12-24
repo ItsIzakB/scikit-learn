45200547889a2b3ddb15f8bc8c5ceb94f6859a8a
izak <kazipriom@gmail.com>
2024-12-24 16:53:14 -0600

    metrics.py

--- python files/metrics.py
+++ python files/metrics.py
@@ -1,6 +1,7 @@
 import kagglehub
 import pandas as pd
 import numpy as np
+
 #
 # path = kagglehub.dataset_download("mlg-ulb/creditcardfraud")
 #
@@ -16,30 +17,26 @@ y = file['Class'].values
 
 from sklearn.linear_model import LogisticRegression
 
-mod = LogisticRegression(class_weight={0:1,1:2}, max_iter=1000)
+mod = LogisticRegression(class_weight={0: 1, 1: 2}, max_iter=1000)
 
-mod.fit(X,y)
-ans  = mod.predict(X).sum
+mod.fit(X, y)
+ans = mod.predict(X).sum
 print(ans)
 
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import GridSearchCV
-from sklearn.metrics import precision_score,recall_score,make_scorer
+from sklearn.metrics import precision_score, recall_score, make_scorer
 
 grid = GridSearchCV(
     estimator=LogisticRegression(max_iter=1000),
     param_grid={'class_weight': [{0: 1, 1: v} for v in range(1, 4)]},
-
-scoring={'precision:': make_scorer(precision_score), 'recall': make_scorer(recall_score)},
+    scoring={'precision': make_scorer(precision_score), 'recall': make_scorer(recall_score)},
+    refit='precision',
+    return_train_score=True,
     cv=4,
     n_jobs=-1
 )
 
-# grid.fit(X,y)
-#
-# print(grid.cv_results_)
-#
-# pd.DataFrame(grid.cv_results_)
-#
-#
-# print(precision_score(y,grid.predict(X)))
+grid.fit(X, y)
+
+print(pd.DataFrame(grid.cv_results_))
